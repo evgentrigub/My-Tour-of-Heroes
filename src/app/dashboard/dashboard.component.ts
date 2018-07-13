46a622dbda0430f1bf8aae25b2b0ec40c5f1bb89
Unknown <evgentrigub@yandex.ru>
2018-07-13 16:41:42 +0300

    Закончил 7 главу

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from "src/app/hero";
+import { HeroService } from "src/app/hero.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  
+  getHeroes(): void{
+    this.heroServices.getHeroes().subscribe(heroes => this.heroes = heroes.slice(1,5));
+  }
 
-  constructor() { }
+  heroes: Hero[] = [];
+
+  constructor(private heroServices: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
